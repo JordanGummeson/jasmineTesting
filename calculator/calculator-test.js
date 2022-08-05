dd88df5ef6fb33474052e60c0a244d69a7719238
Jordan <jordan@LAPTOP-RFTL585C.localdomain>
2022-08-05 13:40:02 -0400

    added testing for calculator

--- calculator/calculator-test.js
+++ calculator/calculator-test.js
@@ -1,11 +1,23 @@
 
 it('should calculate the monthly rate correctly', function () {
   // ...
+  const values = {
+    amount: 41000,
+    rate: 4.5,
+    years: 10
+  }
+  expect(calculateMonthlyPayment(values)).toEqual('424.92')
 });
 
 
 it("should return a result with 2 decimal places", function() {
   // ..
+  const values = {
+    amount: 10043,
+    rate: 5.8,
+    years: 8
+  }
+  expect(calculateMonthlyPayment(values)).toEqual('131.00')
 });
 
 /// etc
